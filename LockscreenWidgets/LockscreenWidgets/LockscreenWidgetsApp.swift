32a332835503c820ffa6bc7f101603ec7e7271d2
paulyanez <34757740+paulyanez@users.noreply.github.com>
2023-06-02 22:04:31 -0400

    Add files via upload

---
+++ LockscreenWidgets/LockscreenWidgets/LockscreenWidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockscreenWidgetsApp.swift
+//  LockscreenWidgets
+//
+//  Created by Paul Yanez on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct LockscreenWidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
